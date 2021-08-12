874541319c19f41afe84176ddff40cfbb05157c5
Angekok <angekok@mail.ru>
2021-08-12 21:28:27 +0300

    Add file name create function.

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,3 +1,13 @@
+"""Load url to files."""
+from re import split, sub
+
+
+def get_name_for_page(page_url):
+    clear_path = sub(r'https://|http://|.html', '', page_url)
+    file_path_list = split(r'[\/\.]', clear_path)
+    clear_file_path_list = [world for world in file_path_list if world]
+    file_path = '-'.join(clear_file_path_list)
+    return '{0}.html'.format(file_path)
 
 
 def download():
