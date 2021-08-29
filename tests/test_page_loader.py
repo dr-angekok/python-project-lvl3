558bf06249f5e77614d2cbf37e954ccb4a53be19
Angekok <angekok@mail.ru>
2021-08-29 20:08:48 +0300

    Add error IO tests.

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -29,21 +29,28 @@ def test_version():
 
 @pytest.mark.parametrize("link,exs", [
     ('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.html'),
-    ('https://pythonworld.ru/moduli/modul-os-path.html', 'pythonworld-ru-moduli-modul-os-path.html'),
-    ('http://pythonworld.ru/moduli/modul-os-path.html', 'pythonworld-ru-moduli-modul-os-path.html'),
-    ('tproger.ru/translations/regular-expression-python', 'tproger-ru-translations-regular-expression-python.html'),
+    ('https://pythonworld.ru/moduli/modul-os-path.html',
+     'pythonworld-ru-moduli-modul-os-path.html'),
+    ('http://pythonworld.ru/moduli/modul-os-path.html',
+     'pythonworld-ru-moduli-modul-os-path.html'),
+    ('tproger.ru/translations/regular-expression-python',
+     'tproger-ru-translations-regular-expression-python.html'),
     ('tproger.ru/translations/regular-expression-python/', 'tproger-ru-translations-regular-expression-python.html')])
 def test_get_name_for_page(link, exs):
     assert page_loader.page_loader.get_name(link) == exs
 
 
 @pytest.mark.parametrize("link,exs,param1,param2", [
-    ('https://pythonworld.ru/moduli/modul-os-path.html', 'pythonworld-ru-moduli-modul-os-path_files', True, False),
-    ('https://pythonworld.ru/moduli/modul-os-path.png', 'pythonworld-ru-moduli-modul-os-path.png', False, True),
+    ('https://pythonworld.ru/moduli/modul-os-path.html',
+     'pythonworld-ru-moduli-modul-os-path_files', True, False),
+    ('https://pythonworld.ru/moduli/modul-os-path.png',
+     'pythonworld-ru-moduli-modul-os-path.png', False, True),
     ('pythonworld.ru/moduli/modul-os-path.jpg', 'pythonworld-ru-moduli-modul-os-path.jpg', False, True)])
 def test_get_name_for(link, exs, param1, param2):
-    assert page_loader.page_loader.get_name(link, is_folder=param1, is_files=param2) == exs
-    assert page_loader.page_loader.get_name(link, is_folder=param1, is_files=param2) == exs
+    assert page_loader.page_loader.get_name(
+        link, is_folder=param1, is_files=param2) == exs
+    assert page_loader.page_loader.get_name(
+        link, is_folder=param1, is_files=param2) == exs
 
 
 def test_cli_help_string():
@@ -61,8 +68,10 @@ def test_download_page(tmpdir):
     PATH_TO_PAGE = 'ru-wikipedia-org-wiki-Python.html'
     PAGE_RESPONSE = read_out_exs('input_page.html')
 
-    page_mock = pook.get(PAGE_LINK_FOR_TEST, reply=200, response_body=PAGE_RESPONSE)
-    image_mock = pook.get(IMAGE_LINK_FOR_TEST, reply=200, response_body='image data test str')
+    page_mock = pook.get(PAGE_LINK_FOR_TEST, reply=200,
+                         response_body=PAGE_RESPONSE)
+    image_mock = pook.get(IMAGE_LINK_FOR_TEST, reply=200,
+                          response_body=PAGE_RESPONSE)
     page_loader.download(PAGE_LINK_FOR_TEST, PATH_TO_FOLDER)
     assert page_mock.calls == 1
     assert path.isfile('{0}/{1}'.format(PATH_TO_FOLDER, PATH_TO_PAGE))
@@ -75,3 +84,33 @@ def test_update_links():
     assert page_loader.page_loader.update_links(input_page,
                                                 'ru-hexlet-io-courses',
                                                 'ru-hexlet-io-courses_files')[0].decode('utf-8') == output_page
+
+
+def test_make_dir_error(tmpdir):
+    PATH_TO_FOLDER = 'ru-wikipedia-org-wiki-Python'
+    with pytest.raises(page_loader.page_loader.MakeDirError):
+        page_loader.page_loader.make_folder(PATH_TO_FOLDER)
+        page_loader.page_loader.make_folder('//incorrect//dir')
+
+
+def test_save_page_error(tmpdir):
+    PAGE = read_out_exs('input_page.html')
+    PATH_TO_PAGE = 'ru-wikipedia-org-wiki-Python.html'
+    PATH = path.join(tmpdir, PATH_TO_PAGE)
+    page_loader.page_loader.save_page(PATH, bytes(PAGE, "utf8"))
+    with pytest.raises(page_loader.page_loader.SavePageError):
+        page_loader.page_loader.save_page(PATH, bytes(PAGE, "utf8"))
+        page_loader.page_loader.save_page('//incorrect//file.path', bytes(PAGE, "utf8"))
+
+
+@pook.on
+@pytest.mark.parametrize("page_link,file_link,rep", [
+    ('htts://pythonworld.ru/moduli/modul-os-path.html',
+     'pythonworld-ru-moduli-modul-os-path_files', 404)])
+def test_load_page_error(page_link, file_link, rep, tmpdir):
+    PAGE_RESPONSE = read_out_exs('input_page.html')
+    pook.get(page_link, reply=rep, response_body=PAGE_RESPONSE)
+    inquiry = []
+    inquiry.append([page_link, path.join(tmpdir, file_link)],)
+    with pytest.raises(page_loader.page_loader.LoadFileError):
+        page_loader.page_loader.save_files(inquiry)
