3ec0f29bc122b831bd56342cd2e1d1c0d1eb55a3
Angekok <angekok@mail.ru>
2021-09-04 20:57:42 +0300

    fix (rf'{html_file_path}', process.stdout) err.

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -12,7 +12,8 @@ def main():
     """Page dounload programm."""
     path_to_page, path_to_folder, log_level = get_arguments()
     try:
-        download(path_to_page, path_to_folder, log_level=log_level)
+        page_filename = download(path_to_page, path_to_folder, log_level=log_level)
+        print('Page saved at {0}'.format(page_filename))
     except Exception as e:
         logging.error('\nHas error: {0}'.format(e))
         exit(1)
