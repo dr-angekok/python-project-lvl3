e6848f51f3b46e4e10c0c2702995e62183b091c5
Angekok <angekok@mail.ru>
2021-09-09 20:20:22 +0300

    Exception is moved to a separate module.

---
+++ page_loader/errors.py
@@ -0,0 +1,14 @@
+class MakeDirError(Exception):
+    pass
+
+
+class SavePageError(Exception):
+    pass
+
+
+class LoadPageError(Exception):
+    pass
+
+
+class SaveFileError(Exception):
+    pass
