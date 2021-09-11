7532e7bb6aaebe9450370c04a0c8a93b7e19adb9
Angekok <angekok@mail.ru>
2021-09-11 23:03:06 +0300

    Fix lint status.

--- tests/test_url.py
+++ tests/test_url.py
@@ -8,8 +8,7 @@ from page_loader.url import to_filename, to_foldername, to_page_filename
     ('https://pythonworld.ru/moduli/modul-os-path.html', 'pythonworld-ru-moduli-modul-os-path.html'),
     ('http://pythonworld.ru/moduli/modul-os-path.html', 'pythonworld-ru-moduli-modul-os-path.html'),
     ('tproger.ru/translations/regular-expression-python', 'tproger-ru-translations-regular-expression-python.html'),
-    ('tproger.ru/translations/regular-expression-python/', 'tproger-ru-translations-regular-expression-python.html'),
-    ])
+    ('tproger.ru/translations/regular-expression-python/', 'tproger-ru-translations-regular-expression-python.html')])
 def test_get_name_for_page(link, exs):
     assert to_page_filename(link) == exs
 
@@ -17,8 +16,7 @@ def test_get_name_for_page(link, exs):
 @pytest.mark.parametrize("link,exs", [
     ('https://pythonworld.ru/moduli/modul-os-path.png', 'pythonworld-ru-moduli-modul-os-path.png'),
     ('pythonworld.ru/moduli/modul-os-path.jpg', 'pythonworld-ru-moduli-modul-os-path.jpg'),
-    ('pythonworld.ru/moduli/modul-os-path', 'pythonworld-ru-moduli-modul-os-path.html'),
-    ])
+    ('pythonworld.ru/moduli/modul-os-path', 'pythonworld-ru-moduli-modul-os-path.html')])
 def test_get_name_for_file(link, exs):
     assert to_filename(link) == exs
 
