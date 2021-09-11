effbe1528623cf723f5df444b1c1fbd4104ff4cb
Angekok <angekok@mail.ru>
2021-09-11 22:34:43 +0300

    Add new separate tests.

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,9 +1,8 @@
 """Test module fore the page_loader."""
-from os import path, popen, listdir
+from os import path
 
 import page_loader
 import pook
-import pytest
 
 
 def make_path(file):
@@ -17,53 +16,8 @@ def read_out_exs(filename):
     return str(out)
 
 
-def load_image(filename):
-    with open(make_path(filename), 'rb') as file:
-        out = file.read()
-    return out
-
-
 def test_version():
     assert page_loader.__version__ == '0.1.0'
-
-
-@pytest.mark.parametrize("link,exs", [
-    ('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.html'),
-    ('https://pythonworld.ru/moduli/modul-os-path.html',
-     'pythonworld-ru-moduli-modul-os-path.html'),
-    ('http://pythonworld.ru/moduli/modul-os-path.html',
-     'pythonworld-ru-moduli-modul-os-path.html'),
-    ('tproger.ru/translations/regular-expression-python',
-     'tproger-ru-translations-regular-expression-python.html'),
-    ('tproger.ru/translations/regular-expression-python/', 'tproger-ru-translations-regular-expression-python.html')])
-def test_get_name_for_page(link, exs):
-    assert page_loader.page_loader.get_name(link) == exs
-
-
-@pytest.mark.parametrize("link,exs,param1,param2", [
-    ('https://pythonworld.ru/moduli/modul-os-path.html',
-     'pythonworld-ru-moduli-modul-os-path_files', True, False),
-    ('https://pythonworld.ru/moduli/modul-os-path.png',
-     'pythonworld-ru-moduli-modul-os-path.png', False, True),
-    ('pythonworld.ru/moduli/modul-os-path.jpg', 'pythonworld-ru-moduli-modul-os-path.jpg', False, True),
-    ('pythonworld.ru/moduli/modul-os-path', 'pythonworld-ru-moduli-modul-os-path.html', False, True)])
-def test_get_name_for(link, exs, param1, param2):
-    assert page_loader.page_loader.get_name(
-        link, is_folder=param1, is_files=param2) == exs
-    assert page_loader.page_loader.get_name(
-        link, is_folder=param1, is_files=param2) == exs
-
-
-def test_cli_help_string():
-    result = str(popen('poetry run page-loader -h').read())
-    assert '-o OUTPUT, --output OUTPUT' in result
-    assert 'positional arguments:\n  link' in result
-    assert 'debug,info,warning,error,critical' in result
-
-
-def test_cli_log_w_err(tmpdir):
-    popen('poetry run page-loader -o {0} http://badsite'.format(tmpdir)).read()
-    assert not listdir(tmpdir)
 
 
 @pook.on
@@ -92,20 +46,3 @@ def test_update_links():
                                                 'tmp/ru-hexlet-io-courses_files',
                                                 'ru-hexlet-io-courses_files',
                                                 '')[0] == output_page
-
-
-def test_make_dir_error(tmpdir):
-    PATH_TO_FOLDER = 'ru-wikipedia-org-wiki-Python'
-    with pytest.raises(page_loader.page_loader.MakeDirError):
-        page_loader.page_loader.make_folder(PATH_TO_FOLDER)
-        page_loader.page_loader.make_folder('//incorrect//dir')
-
-
-def test_save_page_error(tmpdir):
-    PAGE = read_out_exs('input_page.html')
-    PATH_TO_PAGE = 'ru-wikipedia-org-wiki-Python.html'
-    PATH = path.join(tmpdir, PATH_TO_PAGE)
-    page_loader.page_loader.save_page(PATH, PAGE)
-    with pytest.raises(page_loader.page_loader.SavePageError):
-        page_loader.page_loader.save_page(PATH, PAGE)
-        page_loader.page_loader.save_page('//incorrect//file.path', PAGE)
