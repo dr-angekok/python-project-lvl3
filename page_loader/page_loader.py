d26ae5c2fe2db84d5b8c40c8e3c9f9f4fd0ccdcc
Angekok <angekok@mail.ru>
2021-09-12 15:08:22 +0300

    HTML module separately.

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -2,14 +2,13 @@
 import logging
 from os import path
 from sys import stderr
-from urllib.parse import urlparse, urlunparse
 
 import requests
-from bs4 import BeautifulSoup
 from progress.bar import IncrementalBar
 
 from page_loader import url
 from page_loader.errors import LoadPageError
+from page_loader.html import prepare
 from page_loader.storage import make_folder, save_file, save_page
 
 
@@ -38,53 +37,6 @@ def load_page(link):
     except requests.exceptions.HTTPError:
         raise LoadPageError('Connection failed')
     return page.content
-
-
-def update_links(page, _url, path_to_folder, folder_name, page_file_name):
-    """Redirects links within the page file to a local resource.
-
-    Args:
-        page (str): page for changes
-        url (str): link to page
-        path_to_folder (str): path to folder with files
-
-    Returns:
-        [str, tuple]: page and list of links chain
-    """
-    soup = BeautifulSoup(page, "html.parser")
-    links = soup.find_all(["script", "img", "link"])
-    link_chain = []
-    bar = IncrementalBar('Updating links', max=len(links))
-    for tag in links:
-        for attr in ('href', 'src'):
-            if attr in tag.attrs:
-                link = tag[attr]
-                if url.is_not_out_link(link, _url):
-                    logging.debug('sourse to update_link: {0}'.format((_url, link, folder_name, tag, attr)))
-                    parsed_link = urlparse(link)
-                    link_base, link_path = parsed_link.netloc, parsed_link.path
-                    parsed_url = urlparse(_url)
-                    scheme = parsed_url.scheme
-                    url_base, url_path = parsed_url.netloc, parsed_url.path
-                    link = urlunparse((scheme, url_base, link_path, "", "", ""))
-                    if url_base + url_path == link_base + link_path:
-                        logging.debug('path_to_link: {0}'.format(page_file_name))
-                        tag[attr] = page_file_name
-                        logging.debug('tag[attr]: {0}'.format(tag[attr]))
-                        link_chain.append((link, page_file_name))
-                    else:
-                        extra_file_name = url.to_filename(link)
-                        path_to_extra_file = path.join(path_to_folder, extra_file_name)
-                        path_to_update_file_link = path.join(folder_name, extra_file_name)
-                        logging.debug('path_to_link: {0}'.format(path_to_extra_file))
-                        logging.debug('updated_link: {0}'.format(path_to_update_file_link))
-                        tag[attr] = path_to_update_file_link
-                        logging.debug('tag[attr]: {0}'.format(tag[attr]))
-                        link_chain.append((link, path_to_extra_file))
-        bar.next()
-    bar.finish()
-    changed_page = soup.prettify(formatter='html5')
-    return changed_page, link_chain
 
 
 def save_files(link_chain, page_filename):
@@ -154,7 +106,7 @@ def download(link, folder='', log_level='info'):
     path_to_folder = path.join(folder, folder_name)
     make_folder(path_to_folder)
     logging.info('Starting link update')
-    updated_page, page_files_links = update_links(page, link, path_to_folder, folder_name, page_file_name)
+    updated_page, page_files_links = prepare(page, link, path_to_folder, folder_name, page_file_name)
     logging.info('Saving page')
     save_page(page_file_name, updated_page)
     logging.info('Saving accompanying files')
